17494311869e377e558996c8875265f8c9f3c3ea
Ravi Kudumu <ravikudumu@gmail.com>
2021-02-23 20:19:59 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import Tele_Colors_Utility
-if __name__ == '__main__':
-    pair_number = 1
-    for major_color in Tele_Colors_Utility.major_colors:
-        for minor_color in Tele_Colors_Utility.minor_colors:
-            print(Tele_Colors_Utility.color_pair_to_string(major_color, minor_color, pair_number))
-            pair_number += 1
