c17974b3cfa8561a59654f81877b39115bd61f8c
Ravi Kudumu <ravikudumu@gmail.com>
2021-02-23 11:26:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Tele_Colors_Utility
-pair_number = 1
-for major_color in Tele_Colors_Utility.major_colors:
-    for minor_color in Tele_Colors_Utility.minor_colors:
-        print(Tele_Colors_Utility.color_pair_to_string(major_color, minor_color, pair_number))
-        pair_number += 1
+if __name__ == '__main__':
+    pair_number = 1
+    for major_color in Tele_Colors_Utility.major_colors:
+        for minor_color in Tele_Colors_Utility.minor_colors:
+            print(Tele_Colors_Utility.color_pair_to_string(major_color, minor_color, pair_number))
+            pair_number += 1
